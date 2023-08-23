0ab85f7c98e64d64bc37918d440341ade21e9bff
Shah Me'raj <109169939+ShahMehraj@users.noreply.github.com>
2023-08-23 16:55:22 +0530

    Update and rename impeghd_fft_ifft_rom.h to fft_ifft_rom.h

--- fft_ifft_rom.h
+++ fft_ifft_rom.h
@@ -1,5 +1,5 @@
-#ifndef IMPEGHD_FFT_IFFT_ROM_H
-#define IMPEGHD_FFT_IFFT_ROM_H
+#ifndef FFT_IFFT_ROM_H
+#define FFT_IFFT_ROM_H
 extern const FLOAT32 ia_rad_3_fft_twiddle_re[1155];
 extern const FLOAT32 ia_rad_3_fft_twiddle_im[1155];
 extern const FLOAT32 ia_fft_mix_rad_twid_tbl_336[564];
